09c40c1557c320dc265a0a58da62101b8455be26
Colin Smith <colin.smith@gmail.com>
2015-05-03 21:59:55 -0700

    wip

---
+++ math-api/src/main/java/net/littleredcomputer/math/api/HowdyServlet.java
@@ -0,0 +1,13 @@
+package net.littleredcomputer.math.api;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HowdyServlet extends HttpServlet {
+    @Override public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/plain; charset=utf-8");
+        resp.getWriter().println("hello from java, Colin!");
+    }
+}
