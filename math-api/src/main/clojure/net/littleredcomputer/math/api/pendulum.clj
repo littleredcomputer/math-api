08dae52c63819474baae1dde31e679730656e3bd
Colin Smith <colin.smith@gmail.com>
2015-05-08 18:37:14 -0700

    wip

--- math-api/src/main/clojure/net/littleredcomputer/math/api/pendulum.clj
+++ math-api/src/main/clojure/net/littleredcomputer/math/api/pendulum.clj
@@ -1,13 +1,12 @@
 (ns net.littleredcomputer.math.api.pendulum
-  (:require [ring.util.response :refer [response]]
+  (:require [math.examples.figure-1-7]
+            [ring.util.response :refer [response]]
             [ring.util.servlet :refer [defservice]]
             [clojure.tools.logging :as log]
             [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
             [ring.middleware.json :refer [wrap-json-response]]
             [compojure.core :refer [defroutes GET POST]]
-            [hiccup.core :refer :all]
-            [math.examples.figure-1-7 :as f]
-            )
+            [hiccup.core :refer :all])
   (:gen-class :extends javax.servlet.http.HttpServlet))
 
 (defroutes
@@ -16,7 +15,7 @@
        (log/info "params" params)
        (let [args (for [param [:t :A :omega :g :theta0 :thetaDot0]]
                     (Double/valueOf (param params)))]
-         (response (apply f/evolve-pendulum args)))))
+         (response (apply evolve-pendulum args)))))
 
 (defservice (-> pendulum
                 wrap-json-response
