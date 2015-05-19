3794911f69aae0c5e6b26f9b58890867647eada1
Colin Smith <colin.smith@gmail.com>
2015-05-19 08:39:36 -0700

    wip

--- service/src/net/littleredcomputer/math/api/pendulum.clj
+++ service/src/net/littleredcomputer/math/api/pendulum.clj
@@ -14,6 +14,7 @@
   (:import [com.google.appengine.api.memcache MemcacheServiceFactory MemcacheService AsyncMemcacheService])
   (:gen-class :extends javax.servlet.http.HttpServlet))
 
+(set! *warn-on-reflection* true)
 (def ^AsyncMemcacheService memcache-service (MemcacheServiceFactory/getAsyncMemcacheService))
 
 (defroutes
