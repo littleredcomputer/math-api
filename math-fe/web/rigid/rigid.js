bd29c2e31418d8b13c0853dff71f216b5836592b
Colin Smith <colin.smith@gmail.com>
2015-05-26 08:26:07 -0700

    more on rigid. (Time to refactor graph drawing, eh?)

--- math-fe/web/rigid/rigid.js
+++ math-fe/web/rigid/rigid.js
@@ -1,8 +1,25 @@
 function rigid_animation($log) {
-  var animation, renderer, scene, camera, cube;
+  var animation, renderer, scene, camera, cube,
+    pi = Math.PI;
+  function wrap_pi(angle) {
+    var pi = Math.PI;
+    var a = angle;
+    if (-pi > angle || angle >= pi) {
+      a = angle - 2 * pi * Math.floor(angle / 2.0 / pi);
+      a = a < pi ? a : a - 2 * pi;
+    }
+    return a;
+  }
   function setup() {
+    setup_animation();
+    setup_graph();
+  }
+  function setup_graph() {
+  }
+  function setup_animation() {
     console.log('setup');
     animation = document.getElementById('rigid-animation');
+    $log.debug('animation container w/h', animation.offsetWidth, animation.offsetHeight);
     renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize(animation.offsetWidth, animation.offsetHeight);
     animation.appendChild(renderer.domElement);
@@ -10,7 +27,7 @@ function rigid_animation($log) {
     console.log('scene', scene);
     camera = new THREE.PerspectiveCamera(45, animation.offsetWidth / animation.offsetHeight, 1, 10000);
     var origin = new THREE.Vector3(0, 0, 0);
-    camera.position.set(8,8,9);
+    camera.position.set(2,2,2);
     camera.lookAt(origin);
     camera.updateProjectionMatrix();
     $log.debug('camera', camera);
@@ -42,18 +59,90 @@ function rigid_animation($log) {
     cube.rotateX(Math.PI/5);
     cube.rotateY(Math.PI/5);
     scene.add(cube);
-    run();
   }
 
-  function draw(data) {
+  function draw(data, parameters) {
     $log.debug('data received');
 
+    var margin = { left: 40, right: 20, top: 20, bottom: 25},
+      c = document.getElementById('rigid-graph'),
+      cWidth = c.clientWidth,
+      cHeight = c.clientHeight,
+      width = cWidth - margin.left - margin.right,
+      height = cHeight - margin.top - margin.bottom,
+      x_scale = d3.scale.linear().domain([0, parameters.t]).range([0, width]),
+      y_scale = d3.scale.linear().domain([-pi, pi]).range([height, 0]),
+      x_axis = d3.svg.axis().scale(x_scale).orient('bottom'),
+      y_axis = d3.svg.axis().scale(y_scale).orient('left');
+
+    d3.select('#rigid-graph svg').remove();
+    var svg = d3.select('#rigid-graph')
+      .append('svg')
+      .attr('width', cWidth)
+      .attr('height', cHeight)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    svg.append('g')
+      .attr('class','x_axis')
+      .attr('transform','translate(0,'+height+')')
+      .call(x_axis);
+    svg.append('g')
+      .attr('class', 'y_axis')
+      .call(y_axis);
+
+
+    svg.selectAll('circle.theta')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return x_scale(d[0]);
+      })
+      .attr('cy', function(d) {
+        return y_scale(wrap_pi(d[1]));
+      })
+      .classed('graph-point theta', true)
+      .attr('r', 1)
+      .style('fill', '#400');
+
+    svg.selectAll('circle.phi')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return x_scale(d[0]);
+      })
+      .attr('cy', function(d) {
+        return y_scale(wrap_pi(d[2]));
+      })
+      .classed('graph-point phi', true)
+      .attr('r', 1)
+      .style('fill', '#040');
+
+    svg.selectAll('circle.psi')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', function(d) {
+        return x_scale(d[0]);
+      })
+      .attr('cy', function(d) {
+        return y_scale(wrap_pi(d[3]));
+      })
+      .classed('graph-point psi', true)
+      .attr('r', 1)
+      .style('fill', '#004');
   }
 
   function animate($interval, data, parameters) {
     $log.debug('animate');
     var i = 0;
     var t0 = new Date();
+    var theta_points = d3.selectAll('#rigid-graph circle.theta')[0];
+    var phi_points = d3.selectAll('#rigid-graph circle.phi')[0];
+    var psi_points = d3.selectAll('#rigid-graph circle.psi')[0];
+
     var timer = $interval(function() {
       var d = data[i];
       // convert from ZXZ euler angles to rotation matrix
@@ -70,12 +159,22 @@ function rigid_animation($log) {
       cube.rotation.setFromRotationMatrix(m);
       //cube.rotation.set(i,0,0,"XYZ");
       renderer.render(scene, camera);
+
+      theta_points[i].setAttribute('r', 3);
+      phi_points[i].setAttribute('r', 3);
+      psi_points[i].setAttribute('r', 3);
+      if (i > 0) {
+        theta_points[i-1].setAttribute('r', 1);
+        phi_points[i-1].setAttribute('r', 1);
+        psi_points[i-1].setAttribute('r', 1);
+      }
+
       ++i;
     }, 1000 * parameters.dt, data.length, false);
     timer.then(function() {
-      console.log('interval complete');
+      $log.debug('interval complete');
       var ms = new Date() - t0;
-      console.log(data.length, 'points in', ms, 'msec', 1000 * data.length/ms, 'Hz');
+      $log.debug(data.length, 'points in', ms, 'msec', 1000 * data.length/ms, 'Hz');
     });
     return timer;
   }
@@ -125,4 +224,3 @@ angular.module('Rigid', ['ngMaterial', 'ngSanitize', 'cmServices'])
     }
   }]);
 
-console.log('rigid read');
